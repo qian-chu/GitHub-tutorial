f761e7964bdd779d1efba45e62feac1d4077a93a
Qian Chu <qianchu99@gmail.com>
2023-10-03 12:59:00 -0400

    Update everything

--- demo_test.py
+++ demo_test.py
@@ -1,6 +1,6 @@
-from my_func import my_plus, my_minus, my_times
+from demo_func import my_plus, my_minus, my_times
 
-assert my_plus(1, 2) == 1 + 2
+assert my_plus(1, 2)  == 1 + 2
 assert my_minus(1, 2) == 1 - 2
 assert my_times(1, 2) == 1 * 2
 
